c95bd20aeea783cce3263c9d350ac510636cc4ac
Marcelo <mcguzmanr@gmail.com>
2018-09-20 13:12:43 -0400

    Modifications de fichier footer.php

--- vues/header.php
+++ vues/header.php
@@ -19,16 +19,41 @@
                         <i class="fa d-inline fa-lg"></i>
                         <b>Game Logo</b>
                     </a>
-                    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
-
+                    <!-- <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent">
+                        <span class="navbar-toggler-icon"></span>                      
+                    </button> -->
+                    <!-- <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
                         <a class="btn navbar-btn ml-2 text-white btn-secondary">
                         <i class="fa d-inline fa-lg fa-user-circle-o"></i> Se connecter</a>
+                    </div> -->
+
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+                        <i class="fa d-inline fa-lg fa-user-circle-o"></i>
+                        Se connecter
+                    </button>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    ...
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </nav>
-            
         </header>
 <!--    laifu-->
