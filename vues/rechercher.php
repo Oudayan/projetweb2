ea8817e8f7e66fb8d8f180e7c033be752c6786b4
Guilherme <contact@guilhermetosin.com>
2018-10-05 20:25:07 -0400

    Ajoute de le calendrier

--- vues/rechercher.php
+++ vues/rechercher.php
@@ -44,6 +44,7 @@
                                 </div>
                             <?php } ?>
                     </div>
+                    <input type="text" name="datesLocation" id="datesLocation">
                     <button type="submit" class="btn btn-success ml-4">Chercher</button>
                     <a href="index.php?Jeux&action=resetRecherche" class="btn btn-warning ml-4">Reset recherche</a>
             </form>
@@ -90,4 +91,58 @@
     </div>
 
 
+<script>
+    $('#datesLocation').daterangepicker({
+        "showDropdowns": true,
+        "autoApply": true,
+        "dateLimit": {
+            "months": 3
+        },
+        "locale": {
+            "direction": "ltr",
+            "format": "YYYY-MM-DD",
+            "separator": "  au  ",
+            "applyLabel": "Sélectionner",
+            "cancelLabel": "Annuler",
+            "fromLabel": "Du",
+            "toLabel": "Au",
+            "customRangeLabel": "Sur mesure",
+            "daysOfWeek": [
+                "Di",
+                "Lu",
+                "Ma",
+                "Me",
+                "Je",
+                "Ve",
+                "Sa"
+            ],
+            "monthNames": [
+                "Janvier",
+                "Février",
+                "Mars",
+                "Avril",
+                "Mai",
+                "Juin",
+                "Juillet",
+                "Août",
+                "Septembre",
+                "Octobre",
+                "Novembre",
+                "Décembre"
+            ],
+            "firstDay": 1
+        },
+        // Si la disponibilité dateDebut est assignée en $_SESSION et qu'elle dépasse ou est égale à la date d'aujourd'hui, mettre cette date comme date minimum, sinon la date minimum est égale à aujourd'hui
+<!--        --><?//= (isset($_SESSION['disponibilite']['dateDebut']) && strtotime($_SESSION['disponibilite']['dateDebut']) >= date("Y-m-d") ? '"minDate": "' . $_SESSION['disponibilite']['dateDebut'] . '", ' : '"minDate": new Date(), ') ?>
+//        // Si la disponibilité dateFin est assignée en $_SESSION, mettre cette date comme date maximum, sinon, pas de date maximum
+//        <?//= (isset($_SESSION['disponibilite']['dateFin'])) ? '"maxDate": "' . $_SESSION['disponibilite']['dateFin'] . '", ' : '') ?>
+//        // Si la date de début du formulaire recherche est assigné en $_SESSION, mettre cette date comme date de début de la sélection, sinon à la date d'aujourd'hui
+//        <?//= (isset($_SESSION['recherche']['debutLocation']) ? '"startDate": "' . $_SESSION['recherche']['debutLocation'] . '", ' : ' "startDate": "' . strtotime(date("Y-m-d")) . '", ' ?>
+//        // Si la date de fin du formulaire recherche est assigné en $_SESSION, mettre cette date comme date de fin de la sélection, sinon à la date de demain
+//        <?//= (isset($_SESSION['recherche']['finLocation']) ? '"endDate": "' . $_SESSION['recherche']['finLocation'] . '", ' : '"endDate": "' . strtotime(date("Y-m-d") + 1 day) . '", ' ?>
+//        "applyClass": "btn-orange"
+    }, function(start, end, label) {
+        console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
+    });
+</script>
 
